44ff1708cb1749105f082e42d73368d52578b99b
Huygato <giahuynguyen9700@gmail.com>
2025-12-11 21:59:58 +0700

    update them trang thai task

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,10 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-export const connectDB = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGODB_CONNECTIONSTRING);
-        console.log("Liên kết CSDL thành công!");
-
-
-    } catch(error){
-        console.error("Lỗi khi kết nối CSDL:",error);
-        process.exit(1);//exit with error
-        }
-    };
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Kết nối CSDL thành công");
+  } catch (error) {
+    console.error("Lỗi khi kết nối CSDL:", error);
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,25 @@
 import express from "express";
-import taskRoute from "./routes/tasksRouters.js";
+import taskRoutes from "./routes/tasksRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
 
 dotenv.config();
-
+console.log("ENV CHECK → MONGO_URI:", process.env.MONGO_URI);
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 const app = express();
 
-// middlewares
 app.use(express.json());
 
 if (process.env.NODE_ENV !== "production") {
   app.use(cors({ origin: "http://localhost:5173" }));
 }
 
-app.use("/api/tasks", taskRoute);
+app.use("/api/tasks", taskRoutes);
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
